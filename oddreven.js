a3924a445b1176b5af858cc814b65a39e36bfcaa
onlysparrow-web <104550421+onlysparrow-web@users.noreply.github.com>
2022-06-07 17:53:11 +0530

    Add files via upload

---
+++ oddreven.js
@@ -0,0 +1,11 @@
+let a;
+function oddoreven(a) {
+  if (a % 2 == 0) {
+    output = "The given number is even number";
+  } else {
+    output = "The given number is odd number";
+  }
+  return output;
+}
+var check = oddoreven(33);
+console.log(check);
